fe55ee647e987d0e6a221b4ee09b8a02c254e676
Stephen Karger <stephen.karger@gmail.com>
2012-03-14 23:05:33 -0400

    started strassen files

---
+++ pa2/strassen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <errno.h>
+
+#include "utils.h"
+
+int main(int argc, char **argv) {
+    if (argc != 4)
+        error(1,"usage: strassen flag dimension inputfile","");
+
+
+    return 0;
+}
