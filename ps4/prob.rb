d1140581858560b035e89fbcbf8a671766c5ec77
Stephen Karger <stephen.karger@gmail.com>
2012-04-07 00:17:48 -0400

    prob.rb

--- ps4/prob.rb
+++ ps4/prob.rb
@@ -10,22 +10,22 @@ class Integer
   end
 end
 
-r = 0.37
-max = 14
+r = 0.01
+max = 6 
 
-#while r <= 1.0 do
+while r <= 1.0 do
     p = 0
-    i = 0
+    i = 2
     while i <= max do
         eq1 = r**i
         eq2 = (1-r)**(max-i)
         eq3 = max.choose(i)
-        #p += eq1 * eq2 * eq3
-        eq4 = i / 14.0
-        p += eq1 * eq2 * eq3 * eq4
+        p += eq1 * eq2 * eq3
+        #eq4 = i / 14.0
+        #p += eq1 * eq2 * eq3 * eq4
         
-        puts "#{r}\t#{i}\t#{p}"
         i = i+1
     end
-#    r += 0.01
-#end
+    puts "#{r}\t#{p}"
+    r += 0.01
+end
