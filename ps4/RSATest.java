b34b4cce2eeb0ea48e7d054998075fa9fd183519
Stephen Karger <stephen.karger@gmail.com>
2012-04-07 23:13:49 -0400

    1, 2, 3, 5, 6 done

--- ps4/RSATest.java
+++ ps4/RSATest.java
@@ -3,6 +3,7 @@ import java.math.BigInteger;
 public class RSATest {
 
     public static void main(String args[]) {
+/*
         BigInteger n = new BigInteger("46947848749720430529628739081");
         BigInteger e = new BigInteger("37267486263679235062064536973");
         String binx = "100011111010011110110110010101000001" +
@@ -14,5 +15,31 @@ public class RSATest {
 
         BigInteger eofx = x.modPow(e, n);        
         System.out.println(eofx);
+*/
+
+        BigInteger n = new BigInteger("294409");
+        BigInteger n_1 = n.subtract(new BigInteger("1"));
+        BigInteger a = new BigInteger("2");
+        //BigInteger a = new BigInteger("7");
+        BigInteger ax;
+        BigInteger ax_1;
+        BigInteger u = new BigInteger("36801");
+        BigInteger two = new BigInteger("2");
+        BigInteger one = new BigInteger("1");
+        BigInteger negone = new BigInteger("-1");
+	while (u.compareTo(n_1) < 0) {
+		ax_1 = a.modPow(u,n);	
+		System.out.println("u "+ u);
+		u = u.multiply(two);
+		System.out.println("u "+ u);
+		ax = a.modPow(u,n);	
+		System.out.println("ax_1 " + ax_1 + " ax " + ax);
+		if ((!ax_1.equals(one) && !ax_1.equals(negone)) && ax.equals(one) ) {
+			System.out.println("composite: ");
+			System.out.println("ax_1 " + ax_1 + " ax " + ax);
+		}
+	}
+
+
     }
 }
--- ps4/prob.rb
+++ ps4/prob.rb
@@ -10,22 +10,43 @@ class Integer
   end
 end
 
+# 1
+# experimentally show that probability of
+# super-sketch matches is r
+r = 0.9
+max = 14
+p = 0
+i = 0
+while i <= max do
+    eq1 = max.choose(i)
+    eq2 = r**i
+    eq3 = (1-r)**(max-i)
+    eq4 = i / Float(max)
+    p += eq1 * eq2 * eq3 * eq4
+    puts "#{i}\t#{p}"
+    i = i+1
+end
+puts ""
+
+
+
+# 2
+# calculate probability of 2 of more
+# super-sketch matches out of 6
+# across a range or r values
 r = 0.01
 max = 6 
-
 while r <= 1.0 do
     p = 0
     i = 2
     while i <= max do
-        eq1 = r**i
-        eq2 = (1-r)**(max-i)
-        eq3 = max.choose(i)
+    	eq1 = max.choose(i)
+	eq2 = r**i
+        eq3 = (1-r)**(max-i)
         p += eq1 * eq2 * eq3
-        #eq4 = i / 14.0
-        #p += eq1 * eq2 * eq3 * eq4
-        
         i = i+1
     end
     puts "#{r}\t#{p}"
     r += 0.01
 end
+
